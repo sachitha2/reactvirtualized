f946a9a2ecdd03a2671e944b383aa35a20fc8ff9
Sachitha Hirushan <shpsachitha@gmail.com>
2021-05-22 17:57:50 +0530

    last

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{useState,useEffect} from 'react';
+import {
+  List,
+  AutoSizer,
+  CellMeasurer,
+  CellMeasurerCache,
+} from 'react-virtualized';
+import faker from 'faker';
+import './app.css';
 function App() {
+  const [people,setPeole] = useState([]);
+  const [time, setTime] = useState(new Date())
+  const cache = React.useRef(new CellMeasurerCache({
+    fixedWidth:true,
+    defaultHeight:100,
+
+  }));
+
+  useEffect(()=>{
+    setPeole(
+      [...Array(50).keys()].map(key => {
+        return{
+          id:key,
+          name:`${faker.name.firstName()} ${faker.name.lastName()}`,
+          bio:faker.lorem.lines(Math.random() * 100)
+        }
+      })
+    )
+    
+  },[])
+
+
+  useEffect(()=>{
+    const interval = setInterval(()=>{
+      setTime(new Date())
+    },1000)
+    return ()=>clearInterval(interval);
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <div style={{width:"calc(100% - 40px)",height:"calc(100vh - 40px)",backgroundColor:"#333E5F",color:"#fff",margin:"10px",padding:"10px",borderRadius:"10px"}}>
+        <AutoSizer>
+          {
+            ({width,height})=>(
+              <List
+            width={width}
+            height={height}
+            rowHeight={cache.current.rowHeight}
+            deferredMeasurementCache={cache.current}
+            rowCount={people.length}
+            rowRenderer={({key,index,style,parent})=>{
+              const person = people[index];
+              return (
+                <CellMeasurer  key={key} cache={cache.current} parent={parent} columnIndex={0} rowIndex={index}>
+                    <div style={style} className="hello">
+                      <h1>{person.name}</h1>
+                      <p>{JSON.stringify(style)}</p>
+                    </div>
+                </CellMeasurer>
+                    
+                  )
+            }}
+          />
+            )
+          }
+        </AutoSizer>
+      </div>
+      {/* <ul>
+        {people.map(person => <li key={person.id}>
+          <h2>{person.name}</h2>
+        </li>)}
+      </ul> */}
     </div>
   );
 }
